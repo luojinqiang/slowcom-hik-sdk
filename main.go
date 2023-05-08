ab143f66be35002db313c9542e6266f07cb1df27
luojinqiang <luojinqiang168@gmail.com>
2023-05-08 09:37:53 +0800

    xxx

--- main.go
+++ main.go
@@ -10,8 +10,8 @@ import (
 func main() {
 	hikClient := &http.HikHttpClient{
 		BaseUrl:      config.BaseUrl,
-		ClientId:     "e69535b3f3b04b1ea51437d92dcf7b80",
-		ClientSecret: "e9aaa9408d6343a1945aa420074375dc",
+		ClientId:     "123456465",
+		ClientSecret: "134313213433",
 	}
 	deviceControlRequest := service.DeviceControlRequest{HikClient: hikClient}
 
